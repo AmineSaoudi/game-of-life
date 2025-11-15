f423881c650d0b5ec7ad25208c97bfee8013df69
Amine Saoudi <saoudiamine8@gmail.com>
2025-11-14 20:49:10 -0500

    Setup backend springboot

---
+++ backend/src/test/java/com/_Found/GameOfLife/GameOfLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com._Found.GameOfLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOfLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
