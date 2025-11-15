b78b38ec1a74bc6e67e686497ca3309f3e82b9d6
Skigod401 <mwtanner1234@gmail.com>
2025-11-15 00:34:47 -0500

    added some stuff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Landingpage from './pages/landingpageFolder/landingpage.jsx'
 import Navbar from './pages/landingpageFolder/navbar.jsx'
+import { Toolbar } from '@mui/material';
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Toolbar/>
     <Landingpage/>
       </>
   )
--- frontend/src/pages/landingpageFolder/landingpage.jsx
+++ frontend/src/pages/landingpageFolder/landingpage.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import styles from "./landingpage.module.css"
-import { Box, Typography, Button } from '@mui/material';
+import { Box } from '@mui/material';
+import SingleTasksCard from "../RecurringTaskList/SingleTasksCard.jsx"
+import HabitTasksCard from "../RecurringTaskList/HabitTasksCard.jsx"
+
 
 const LandingPage = () => {
   return (
     <Box className={styles.mainBox}>
-      <Typography> LANDING PAGE START</Typography>
+      <SingleTasksCard currentUser={{ id: 1 }} />
+      <HabitTasksCard currentUser={{ id: 1 }} />
+
+
+
 
         
     </Box>
